6a5568708410df8d58499cade3a7e9484ffc3b44
Aurelien Vincent--randonnier <vnr4817a@ens.fsi>
2023-09-21 11:20:38 +0200

    initial

---
+++ src/personnage/Romains.java
@@ -0,0 +1,14 @@
+package personnage;
+
+public class Romains {
+	private String nom;
+	private int force;
+	
+	public Romains(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	
+}
+
